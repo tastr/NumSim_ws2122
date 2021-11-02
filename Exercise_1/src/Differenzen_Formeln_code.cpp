c2d1c8bd999cdbb670b049aeae472e32b4be033b
chbeck96 <93437883+chbeck96@users.noreply.github.com>
2021-11-02 18:17:31 +0100

    Add files via upload

--- Exercise_1/src/Differenzen_Formeln_code.cpp
+++ Exercise_1/src/Differenzen_Formeln_code.cpp
@@ -39,7 +39,7 @@ for (j = 1; j < Tiefe; j++)
     {
         zwischenwert_viphj    = v(i+1,j)   + v(i,j)   ;
         zwischenwert_viph1jm1 = v(i+1,j-1) + v(i,j-1) ;
-        zwischenwert_uijph    = u(i,j+1)   + u(i,j)   ;
+        zwischenwert_uijph    = u(i,j+1)   + u(i,j)   ; // kann ersetzt werden
         zwischenwert_uijmh    = u(i,j)     + u(i,j-1) ;
         zwischenwert_vimhj    = v(i,j)     + v(i-1,j) ;
         zwischenwert_uim1jph  = u(i-1,j+1) + u(i-1,j) ;
@@ -52,14 +52,14 @@ for (j = 1; j < Tiefe; j++)
         u_xx=delta_x_quadrat_divisor * (u(i+1,j) - 2 * u(i,j) + u(i-1,j));
         u_yy=delta_y_quadrat_divisor * (u(i,j+1) - 2 * u(i,j) + u(i,j-1));
 
-        v_xx = delta_x_quadrat_divisor*(v(i+1,j) - 2 * v(i,j) + v(i-1,j));
-        v_yy = delta_y_quadrat_divisor*(v(i,j+1) - 2 * v(i,j) + v(i,j-1));
+        v_xx = delta_x_quadrat_divisor * (v(i+1,j) - 2 * v(i,j) + v(i-1,j));
+        v_yy = delta_y_quadrat_divisor * (v(i,j+1) - 2 * v(i,j) + v(i,j-1));
 
         u_quadrat_x = delta_x_divisor / 4 * (zwischenwert_uiphj - zwischenwert_uimhj);
         v_quadrat_y = delta_y_divisor / 4 * (zwischenwert_vijph - zwischenwert_vijmh);
 
-        uv_x = 1/4 * delta_x_divisor * (zwischenwert_viphj * zwischenwert_uijph - zwischenwert_vimhj   * zwischenwert_uim1jph);
-        uv_y = 1/4 * delta_y_divisor * (zwischenwert_viphj * zwischenwert_uijph - zwischenwert_viphjm1 * zwischenwert_uijmh);
+        uv_x = delta_x_divisor /4 * (zwischenwert_viphj * zwischenwert_uijph - zwischenwert_vimhj   * zwischenwert_uim1jph);
+        uv_y = delta_y_divisor /4 * (zwischenwert_viphj * zwischenwert_uijph - zwischenwert_viphjm1 * zwischenwert_uijmh);
 
         u_x = delta_x_divisor * (u(i,j)-u(i-1,j));
         v_y = delta_y_divisor * (v(i,j)-v(i,j-1));
@@ -67,7 +67,7 @@ for (j = 1; j < Tiefe; j++)
         //A(i,j)= 1/re * (u_xx(i,j)+u_yy(i,j) - uv_y(i,j) - u_quadrat_x(i,j) + gX;
         //B(i,j)= 1/re * (v_xx(i,j)+v_yy(i,j) - uv_x(i,j) - v_quadrat_y(i,j) + gY;
         
-        F(i,j) = u(i,j)+ delta_t * (1/re * (u_xx(i,j)+u_yy(i,j) - uv_y(i,j) - u_quadrat_x(i,j) + gX);
-        G(i,j) = u(i,j)+ delta_t * (1/re * (v_xx(i,j)+v_yy(i,j) - uv_x(i,j) - v_quadrat_y(i,j) + gY);
+        F(i,j) = u(i,j) + delta_t * ((u_xx(i,j)+u_yy(i,j) - uv_y(i,j) / re - u_quadrat_x(i,j) + gX);
+        G(i,j) = v(i,j) + delta_t * ((v_xx(i,j)+v_yy(i,j) - uv_x(i,j) / re - v_quadrat_y(i,j) + gY);
     }
 }

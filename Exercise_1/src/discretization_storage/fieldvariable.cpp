f75853144c6fec5c5c901f519e4d53d600735017
christian Beck <christian.beck-96@gmx.de>
2021-11-12 12:10:55 +0100

    aktuelleste Version

--- Exercise_1/src/discretization_storage/fieldvariable.cpp
+++ Exercise_1/src/discretization_storage/fieldvariable.cpp
@@ -3,8 +3,8 @@
 
 
 //FieldVariable::FieldVariable(std::array<int,2> size) : 
-FieldVariable::FieldVariable(std::array<int,2> size,std::array<int,2> nCells,std::array<double,2> offset)
-: Array2D(size), offset_(offset),nCells_(nCells)
+FieldVariable::FieldVariable(std::array<int,2> size,Settings settings,std::array<double,2> offset)
+: Array2D(size), offset_(offset),settings_(settings)
 {
     maximum=0;
     absmaximum=0;
@@ -72,32 +72,36 @@ double FieldVariable::interpolateAt(double x, double y)
   double top_average; 
   double bottom_average;
   
-  double dx=1.0/nCells_[0];
-  double dy=1.0/nCells_[1];
+  double dx=settings_.physicalSize[0]/settings_.nCells[0];
+  double dy=settings_.physicalSize[1]/settings_.nCells[1];
 
-  x=x+ offset_[0];
-  y=y+ offset_[1]; 
+  x=x+ offset_[0] * dx;
+  y=y+ offset_[1] * dy; 
  
 
      while (x>(i_right*dx))
     {
         i_right=++i_right;
+       // std::cout << i_right <<std::endl;
     }
     while (y>(dy*j_top))
     { 
-        j_top=++j_top;  
-         
+        j_top=++j_top;         
     }
  
-    propfact_x =x*nCells_[0]-i_right+1;
-    propfact_y =y*nCells_[1]-j_top+1;
+    propfact_x =x*settings_.nCells[0]-i_right+1;
+    propfact_y =y*settings_.nCells[1]-j_top+1;
  
  
-   std::cout << propfact_x <<"  " <<  propfact_y <<std::endl;
+  // std::cout << propfact_x <<"  " <<  propfact_y <<std::endl;
+  
   bottom_average = (1-propfact_x)*data_[indexconvert(i_right-1,j_top-1)] + propfact_x*(1-propfact_x)*data_[indexconvert(i_right,j_top-1)];
   top_average    = (1-propfact_x)*data_[indexconvert(i_right-1,j_top)]   + propfact_x*(1-propfact_x)*data_[indexconvert(i_right,j_top)];
+  
   //std::cout << i_right << "   " << x <<std::endl;
   //std::cout << j_top << "   " << y <<std::endl;
+  //std::cout << j_top << "   "<< i_right <<"  " <<(1-propfact_y) * bottom_average + propfact_y * top_average   << y <<std::endl;
+  
   return  (1-propfact_y) * bottom_average + propfact_y * top_average  ;
    //return 1;
 }

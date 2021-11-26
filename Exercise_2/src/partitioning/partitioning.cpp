393c6cd9ad505f43ece494e49e75af407253a893
christian Beck <christian.beck-96@gmx.de>
2021-11-26 20:16:34 +0100

    Struktur partitioning

--- Exercise_2/src/partitioning/partitioning.cpp
+++ Exercise_2/src/partitioning/partitioning.cpp
@@ -1,35 +1,136 @@
 #include "partitioning.h"
 
-Partitioning::Partitioning(Settings settings)
+Partitioning::Partitioning(Settings settings):
+nCellsGlobal_(settings.nCells)
 {
-    
-}
+    n=3;  // predefined for Tests, use 3x3 since it contains all possible bordercases 
+    m=3;  // in the project these numbers need to be calculated in a function that defines the domainsplitting.
+    setOwnRankNo();//could be given to the partitioning
+    setNodeOffset();
+    setOwnPartitionContainsBottomBoundary();
+    setOwnPartitionContainsLeftBoundary();
+    setOwnPartitionContainsRightBoundary();
+    setOwnPartitionContainsTopBoundary();  
+    setNCells();
+    std::cout<< n << m <<std::endl; 
+    std::cout<< ownRankNoValue <<std::endl;  
+    std::cout<< nodeOffsetValue[0] << nodeOffsetValue[1] << std::endl;  
+    std::cout<<  nCells[0] <<  nCells[1] << std::endl;
+    }
+
+
 
 
     int Partitioning::ownRankNo()
     {
-        return 0;
+        return ownRankNoValue;
+
     }
     int Partitioning::ownPartitionContainsBottomBoundary()
     {
-         return 0;
+         return ownPartitionContainsBottomBoundaryValue;
     }
     int Partitioning::ownPartitionContainsLeftBoundary()
     {
-         return 0;
+         return ownPartitionContainsLeftBoundaryValue;
     }
     int Partitioning::ownPartitionContainsRightBoundary()
     {
-         return 0;
+         return ownPartitionContainsRightBoundaryValue;
     }
     int Partitioning::ownPartitionContainsTopBoundary()
     {
-         return 0;
+         return ownPartitionContainsTopBoundaryValue;
     }
-    std::array<int,2> nodeOffset()
+    std::array<int,2> Partitioning::nodeOffset()
     {
-        return {0,0};
+       return nodeOffsetValue;
     }
+
+
+
+    // Initialization functions
+    void Partitioning::setOwnPartitionContainsBottomBoundary()
+    {
+        int result=0;
+        if (nodeOffsetValue[1]==0)
+        {
+            result=1;
+        }
+        ownPartitionContainsBottomBoundaryValue=result;
+    }
+    void Partitioning::setOwnPartitionContainsLeftBoundary()
+    {
+        int result=0;
+        if (nodeOffsetValue[0]==0)
+        {
+            result=1;
+        }
+        ownPartitionContainsLeftBoundaryValue=result;
+    }
+    void Partitioning::setOwnPartitionContainsRightBoundary()
+    {
+        int result=0;
+        if (nodeOffsetValue[0]==n-1)
+        {
+            result=1;
+        }
+        ownPartitionContainsRightBoundaryValue=result;
+    }
+    void Partitioning::setOwnPartitionContainsTopBoundary()
+    {
+        int result=0;
+        if (nodeOffsetValue[1]==m-1)
+        {
+            result=1;
+        }
+        ownPartitionContainsTopBoundaryValue=result;
+    }
+    void Partitioning::setOwnRankNo()  
+    {
+    //return MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    ownRankNoValue=8;
+    }
+
+    void Partitioning::setNodeOffset()
+    {int i=0;
+     int j=0;
+     
+    while(ownRankNoValue-(j+1)*n>0)
+        {        
+        j++;
+        } 
+    i=ownRankNoValue-j*n;
+    nodeOffsetValue={i,j};  
+    //enumeration from 0 to n-1/m-1
+    }
+
+    void Partitioning::setNCells()
+    {
+        nCells[0]=nCellsGlobal_[0]/n;
+        nCells[1]=nCellsGlobal_[1]/m;
+        if (nodeOffsetValue[0]==n-1)
+        {
+            nCells[0]=nCellsGlobal_[0]-(nodeOffsetValue[0])*nCells[0];
+        }
+        if (nodeOffsetValue[1]==m-1)
+        {
+            nCells[1]=nCellsGlobal_[1]-(nodeOffsetValue[1])*nCells[1];
+        }
+         
+   
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- Exercise_2/src/partitioning/partitioning.h
+++ Exercise_2/src/partitioning/partitioning.h
@@ -3,7 +3,6 @@
 //#include "computation/computation.h"
 #include <cstdlib>
 #include <array>
-
 #include "settings.h"
 
 
@@ -12,6 +11,27 @@ class Partitioning
 {
 protected:
         std::array<int,2> offset();
+        int n; // Number of Subdomains horizontally
+        int m; // Number of Subdomains verticallz
+        
+    int ownPartitionContainsBottomBoundaryValue;
+    int ownPartitionContainsLeftBoundaryValue;
+    int ownPartitionContainsRightBoundaryValue;
+    int ownPartitionContainsTopBoundaryValue;
+    int ownRankNoValue;
+    std::array<int,2> nodeOffsetValue;
+    std::array<int,2> nCellsGlobal_;
+    std::array<int,2> nCells;
+
+    // Initialization functions
+    void setOwnPartitionContainsBottomBoundary();
+    void setOwnPartitionContainsLeftBoundary();
+    void setOwnPartitionContainsRightBoundary();
+    void setOwnPartitionContainsTopBoundary();
+    void setOwnRankNo(); 
+    void setNodeOffset(); 
+    void setNCells();
+
 public:
     Partitioning(Settings settings);
     
@@ -21,4 +41,5 @@ public:
     int ownPartitionContainsRightBoundary();
     int ownPartitionContainsTopBoundary();
     std::array<int,2> nodeOffset();     
+
 };

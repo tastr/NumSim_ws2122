b21d7431c5f3394dee5a0952068e8ad5902bc9b3
christian Beck <christian.beck-96@gmx.de>
2022-01-31 10:17:37 +0100

    leicht kommentierter Code

--- Project/src/Geometry.h
+++ Project/src/Geometry.h
@@ -11,27 +11,74 @@ class Geometry
 private:
   int rownumber;
   int columnnumber;
- 
+  //number of rows from the transfered matrix
   int getRowNumber(std::string filename);
+
+ //number of columns from the transfered matrix
   int getColumnNumber(std::string filename);
-  int getObstacleCount(std::string filename);
-  void writeMatrix(std::string filename);
-  void createGeometry();
+// Matrix with the values from the file
   IntArray2D matrix;
+  // Array that contains the flags of the obsticales
   IntArray2D geometry;
+  // Array with the indices of the obstacle Values 
   IntArray2D obstacleCellsIndices;
+  // Array with the indices of the fluid Values 
   IntArray2D fluidCellsIndices;
+
+
+
+
+/*
+  * returns the number of obstacles based on the original file (for pre constructor initialisation)  
+*/
+  int getObstacleCount(std::string filename);
+
+/*
+  * writes the values from the file to an array  
+*/
+  void writeMatrix(std::string filename);
+
+/*
+  * creates the geometry based on the values of the matrix  
+*/
+void createGeometry();
+
 
 public:
    Geometry(std::string filename);
   ~Geometry();
 
-    
+  /*
+  * prints the Values whom have been read from the file onto the consol
+  * Starts counting in the top left corner
+  */
 void  printMatrix();
+
+
+/*
+  * prints the Values of the geometry
+  * Starts counting in the bottom left corner
+*/
 void printGeometry();
+
+/*
+  * returns the indices of the ith fluid cell out of the vector with fluidcells
+*/
 std::array<int, 2> getFluidCellsIndices(int i) const;
+
+/*
+* returns the length of the fluidcellvector (number of fluidcells)
+*/
 int getLengthFluidCellsIndices(int i) const;
+
+/*
+  * returns the indices of the ith obstacles cell out of the vector with fluidcells
+*/
 std::array<int, 2> getObstacleCellsIndices(int i) const;
+
+/*
+* returns the length of the obstaclecellvector (number of obstaclecells)
+*/
 int getLengthObstacleCellsIndices(int i) const;
 
 
